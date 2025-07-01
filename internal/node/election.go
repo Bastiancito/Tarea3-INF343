c8d2549a56c9d7a7ba991cfaa8a12c27305a81d1
Bastiancito <bastian.camusj@usm.cl>
2025-07-01 00:36:33 -0400

    ss

--- internal/node/election.go
+++ internal/node/election.go
@@ -2,30 +2,16 @@ package node
 
 import (
     "time"
+
     "github.com/Bastiancito/tarea3/internal/transport"
 )
 
 func (n *Node) runLeaderElection() {
-    electionTimer := time.NewTimer(time.Duration(n.cfg.ElectionTimeoutMs) * time.Millisecond)
-    defer electionTimer.Stop()
-
     for {
         select {
         case <-n.electionCh:
             n.log("Se recibió señal de elección")
             n.startElection()
-            electionTimer.Reset(time.Duration(n.cfg.ElectionTimeoutMs) * time.Millisecond)
-
-        case <-electionTimer.C:
-            n.mu.RLock()
-            leader := n.leaderID
-            n.mu.RUnlock()
-            if leader == -1 {
-                n.log("ElectionTimeout sin líder, lanzando nueva elección")
-                n.startElection()
-            }
-            electionTimer.Reset(time.Duration(n.cfg.ElectionTimeoutMs) * time.Millisecond)
-
         case <-n.ctx.Done():
             return
         }
@@ -34,85 +20,55 @@ func (n *Node) runLeaderElection() {
 
 func (n *Node) startElection() {
     n.mu.Lock()
-    defer n.mu.Unlock()
+    n.leaderID = -1
+    n.isLeader = false
+    n.mu.Unlock()
 
-    n.log("Iniciando elección")
-
-    if n.leaderID != -1 && n.leaderID != n.cfg.SelfID {
-        n.log("Ya hay un líder registrado (%d), no inicio elección", n.leaderID)
-        return
+    higherExists := false
+    for peerID := range n.cfg.Peers {
+        if peerID > n.cfg.SelfID {
+            higherExists = true
+            msg := &transport.Envelope{
+                Type: "Election",
+                From: n.cfg.SelfID,
+            }
+            _ = n.transport.Send(peerID, msg)
+        }
     }
 
-    higherNodes := n.getHigherNodes()
-    if len(higherNodes) == 0 {
+    if !higherExists {
         n.becomeLeader()
         return
     }
 
-    msg := &transport.Envelope{
-        Type: "Election",
-        From: n.cfg.SelfID,
-    }
+    timeout := time.NewTimer(time.Duration(n.cfg.ElectionTimeoutMs) * time.Millisecond)
+    defer timeout.Stop()
 
-    responses := make(chan bool, len(higherNodes))
-    for _, id := range higherNodes {
-        go func(peerID int) {
-            err := n.transport.Send(peerID, msg)
-            responses <- (err == nil)
-        }(id)
-    }
-
-    timeout := time.After(time.Duration(n.cfg.ElectionTimeoutMs) * time.Millisecond)
-    receivedResponse := false
-
-WAIT:
-    for i := 0; i < len(higherNodes); i++ {
+    for {
         select {
-        case ok := <-responses:
-            if ok {
-                receivedResponse = true
-            }
-        case <-timeout:
-            break WAIT
+        case <-timeout.C:
+            n.becomeLeader()
+            return
+        case <-n.electionCh:
+            n.log("Se recibió nueva señal de elección")
+            n.startElection()
+            return
+        case <-n.ctx.Done():
+            return
         }
     }
-
-    if receivedResponse {
-        n.log("Esperando que un nodo con mayor ID se proclame líder...")
-        return
-    }
-
-    n.becomeLeader()
 }
 
 func (n *Node) becomeLeader() {
+    n.mu.Lock()
     n.isLeader = true
-    if n.leaderID == n.cfg.SelfID {
-        n.log("Ya soy el líder. Ignorando proclamación redundante.")
-        return
-    }
-    
     n.leaderID = n.cfg.SelfID
-    n.log("¡Elegido como nuevo líder!")
+    n.mu.Unlock()
 
-    msg := &transport.Envelope{
-        Type: "LeaderAnnouncement",
+    n.log("¡Elegido como nuevo líder!")
+    coord := &transport.Envelope{
+        Type: "Coordinator",
         From: n.cfg.SelfID,
     }
-
-    if err := n.transport.Broadcast(msg); err != nil {
-        n.log("Error anunciando liderazgo: %v", err)
-    }
-
-    go n.sendHeartbeats()
-}
-
-func (n *Node) getHigherNodes() []int {
-    var higher []int
-    for id := range n.cfg.Peers {
-        if id > n.cfg.SelfID {
-            higher = append(higher, id)
-        }
-    }
-    return higher
+    n.transport.Broadcast(coord)
 }
