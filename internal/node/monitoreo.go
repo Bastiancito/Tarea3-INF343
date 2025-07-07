aca7852c2c74431b583cca7cd5b44d21024fd306
Bastiancito <bastian.camusj@usm.cl>
2025-07-07 04:38:36 +0000

    finals

--- internal/node/monitoreo.go
+++ internal/node/monitoreo.go
@@ -15,9 +15,17 @@ func (n *Node) monitorLeader() {
     for {
         select {
         case <-ticker.C:
-            if n.leaderID != -1 && n.leaderID != n.cfg.SelfID {
-                alive, err := n.rpcClient.Ping(n.leaderID)
-                if err != nil || !alive {
+            if n.leaderID == n.cfg.SelfID {
+                select {
+                case n.heartbeatCh <- struct{}{}:
+                }
+
+            } else if n.leaderID != -1 {
+                alive,err := n.rpcClient.Ping(n.leaderID)
+                if err != nil {
+                    n.log("Error al hacer ping al líder %d: %v", n.leaderID, err)
+                }
+                if !alive || err!=nil {
                     n.log("El líder %d no responde - iniciando elección", n.leaderID)
                     n.electionCh <- struct{}{}
                 }
